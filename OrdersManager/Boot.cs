3dcfa120e7af832579d5f0ffccf96a5629286851
wfrohmberg <wojciech.frohmberg@cs.put.poznan.pl>
2024-04-19 11:37:01 +0200

    ADD: Extract a section to initialize phase of application

---
+++ OrdersManager/Boot.cs
@@ -0,0 +1,16 @@
+﻿
+namespace OrdersManager
+{
+    public class Boot : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.Factory.StartNew(() => { });
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OrdersManager/Program.cs
+++ OrdersManager/Program.cs
@@ -1,3 +1,4 @@
+using OrdersManager;
 using OrdersManager.DbContexts;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService(services => services.GetService<Boot>());
 
 var app = builder.Build();
 
