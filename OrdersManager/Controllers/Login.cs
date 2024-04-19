812b707e76eaf104fffa12b369f0505ae38bc65a
wfrohmberg <wojciech.frohmberg@cs.put.poznan.pl>
2024-04-19 13:35:14 +0200

    ADD: Attempt to login controller

---
+++ OrdersManager/Controllers/Login.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OrdersManager.Controllers.Models.Login;
+using OrdersManager.DbContexts;
+using OrdersManager.Models;
+using OrdersManager.ModelsActions;
+using System.Runtime.CompilerServices;
+
+namespace OrdersManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Login : ControllerBase
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly UserCredentialsCheck _userCredentialsCheck;
+        public Login(IServiceScopeFactory serviceScopeFactory, UserCredentialsCheck userCredentialsCheck) 
+        { 
+            _scopeFactory = serviceScopeFactory;
+            _userCredentialsCheck = userCredentialsCheck;
+        }
+
+        
+        [HttpPost]
+        public string Post([FromBody] Credentials credentials)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                using (var context = scope.ServiceProvider.GetService<OrdersContext>()!)
+                {
+                    _userCredentialsCheck.Login = credentials.login;
+                    _userCredentialsCheck.Password = credentials.password;
+                    if (!_userCredentialsCheck.Execute())
+                    {
+                        HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        return "";
+                    }
+                    else
+                    {
+                        return "SUCCESS";
+                    }
+
+
+                }
+            }
+        }
+
+    }
+}
---
+++ OrdersManager/Controllers/Models/Login/Credentials.cs
@@ -0,0 +1,8 @@
+﻿namespace OrdersManager.Controllers.Models.Login
+{
+    public class Credentials
+    {
+        public string? login { get; set; }
+        public string? password { get; set; }
+    }
+}
