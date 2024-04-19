c9811948a58678158be65867bcd6f222f2d8c1e0
wfrohmberg <wojciech.frohmberg@cs.put.poznan.pl>
2024-04-19 15:09:41 +0200

    ADD: Token check middleware

--- OrdersManager/Controllers/WeatherForecastController.cs
+++ OrdersManager/Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using OrdersManager.Middleware;
 
 namespace OrdersManager.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [MiddlewareFilter(typeof(TokenMiddleware))]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
---
+++ OrdersManager/Middleware/TokenMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using OrdersManager.ControllersActions;
+
+namespace OrdersManager.Middleware
+{
+    public class TokenMiddleware
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("token"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Response.WriteAsync("UNAUTHORIZED!").Wait();
+                    return;
+                }
+                var tokenExpiration = context.RequestServices.GetService<CheckTokenExpiration>();
+                if (!tokenExpiration.IsValid(context.Request.Headers["token"]!))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Response.WriteAsync("UNAUTHORIZED!").Wait();
+                    return;
+                }
+                
+                await next.Invoke(context);
+            });
+        }
+    }
+}
