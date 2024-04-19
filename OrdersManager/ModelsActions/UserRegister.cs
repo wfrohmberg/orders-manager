c6f080c926d4d090d1addf06b8ccf62754d49323
wfrohmberg <wojciech.frohmberg@cs.put.poznan.pl>
2024-04-19 11:35:50 +0200

    ADD: Action for user registration

---
+++ OrdersManager/ModelsActions/UserRegister.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OrdersManager.DbContexts;
+using OrdersManager.Models;
+using System.Windows.Input;
+
+namespace OrdersManager.ModelsActions
+{
+    public class UserRegister
+    {
+        private readonly OrdersContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public UserRegister(OrdersContext context, IPasswordHasher<User> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+
+        public void Execute()
+        {
+            if (Login == null)
+            {
+                throw new ArgumentNullException(nameof(Login));
+            }
+            if (Password == null)
+            {
+                throw new ArgumentNullException(nameof(Password));
+            }
+
+            var user = new User
+            {
+                Login = Login
+            };
+            user.PasswordHash = _passwordHasher.HashPassword(user, Password);
+            _context.Users.Add(user);
+        }
+    }
+}
