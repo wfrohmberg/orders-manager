0dd3e36ebfcb26c8015ac6a8d951f079f937f059
wfrohmberg <wojciech.frohmberg@cs.put.poznan.pl>
2024-04-20 11:01:22 +0200

    ADD: validation information to model Address

--- OrdersManager/Models/Address.cs
+++ OrdersManager/Models/Address.cs
@@ -1,16 +1,24 @@
-﻿namespace OrdersManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrdersManager.Models
 {
     public class Address
     {
         public int Id { get; set; }
+        [RegularExpression("^\\d{2}-\\d{3}$")]
         public string PostalCode { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string HouseNumber { get; set; }
         public string? FlatNumber { get; set; }
+        [Required]
         public string City { get; set; }
+        [RegularExpression("^(?<!\\w)(\\(?(\\+|00)?48\\)?)?[ -]?\\d{3}[ -]?\\d{3}[ -]?\\d{3}(?!\\w)$")]
         public string? PhoneNumber { get; set; }
+        [RegularExpression("^(?(\")(\".+?(?<!\\\\)\"@)|(([0-9a-z]((\\.(?!\\.))|[-!#\\$%&'\\*\\+/=\\?\\^`\\{\\}\\|~\\w])*)(?<=[0-9a-z])@))(?(\\[)(\\[(\\d{1,3}\\.){3}\\d{1,3}\\])|(([0-9a-z][-\\w]*[0-9a-z]*\\.)+[a-z0-9][\\-a-z0-9]{0,22}[a-z0-9]))$")]
         public string? EmailAddress { get; set; }
-        public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; set; } = false;
 
         public int UserId { get; set; }
         public User User { get; set; }
