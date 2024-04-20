ef509f49b4014c9ef285157d005e27833488517b
wfrohmberg <wojciech.frohmberg@cs.put.poznan.pl>
2024-04-20 18:37:34 +0200

    UPDATE: Migration to add order flags

---
+++ OrdersManager/Migrations/20240420163647_AddOrderFlags.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrdersManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderFlags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Cancelled",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Executed",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cancelled",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Executed",
+                table: "Orders");
+        }
+    }
+}
--- OrdersManager/Models/Order.cs
+++ OrdersManager/Models/Order.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public List<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
+        public bool Cancelled { get; set; } = false;
+        public bool Executed { get; set; } = false;
     }
 }
